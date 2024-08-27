75b9249da9e5ababacc4e59b3891daddacfe819a
pathaksrabi <pathaksrabi@gmail.com>
2024-08-27 14:24:07 -0400

    1st commit

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,9 @@
-# add your code here
-
+for i in range (1, 101): # loops through the numbers 1 to 100
+    if i % 3 == 0 and i % 5 == 0: # checks if the number is divisible by both 3 and 5
+        print ("FizzBuzz")
+    elif i % 3 == 0: # checks if the number is divisible by 3
+        print ("Fizz")
+    elif i % 5 == 0: # checks if the number is divisible by 5
+        print ("Buzz") 
+    else: # if none of the above is true, print the number itself
+        print (i)
